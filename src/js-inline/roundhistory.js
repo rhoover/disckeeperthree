65921c053f86d8ef3e820022faf2f46a71765442
Robin Hoover <robin.hoover@gmail.com>
2022-11-14 15:09:43 -0500

    Round History

--- src/js-inline/roundhistory.js
+++ src/js-inline/roundhistory.js
@@ -14,8 +14,33 @@
             return
           } else {
             console.log(data);
+            data.sort((a,b) => {
+              return new Date(b.date) - new Date(a.date);
+            });
+            roundhistory.buildRoundsList(data);
           }
         });
+    },
+
+    buildRoundsList(roundsData) {
+      let roundsList = document.querySelector('.round-history');
+      let roundsOutput = "";
+
+      roundsData.forEach(function(round) {
+
+        roundsOutput += 
+          `<div class="round-history-round">
+            <p class="round-history-round-header">${round.course} <span>${round.date}</span></p>
+            <div class="round-history-round-data">
+              ${round.players.map(player => 
+              `<p class="round-history-round-data-name">${player.nameFirst} ${player.nameLast}</p>
+              <p class="round-history-round-data-score"><span>${player.holes[17].overUnderRound}</span> from ${player.holes[17].throwsRound} throws</p>`).join('\n').replaceAll('"', '')}
+            </div>
+          </div>`;
+      });
+
+      roundsList.innerHTML = roundsOutput;
+
     }
 
 
