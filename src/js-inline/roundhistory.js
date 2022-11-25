5016c8cef7de5fb25374807ebd158b070eb5aa39
Robin Hoover <robin.hoover@gmail.com>
2022-11-25 12:40:17 -0500

    Tweaking Display

--- src/js-inline/roundhistory.js
+++ src/js-inline/roundhistory.js
@@ -25,16 +25,17 @@
     buildRoundsList(roundsData) {
       let roundsList = document.querySelector('.round-history');
       let roundsOutput = "";
+      console.log(roundsData);
 
       roundsData.forEach(function(round) {
 
         roundsOutput += 
           `<div class="round-history-round">
-            <p class="round-history-round-header">${round.course} <span>${round.date}</span></p>
+            <p class="round-history-round-header">${round.course} <span>${round.roundDate}</span></p>
             <div class="round-history-round-data">
               ${round.players.map(player => 
               `<p class="round-history-round-data-name">${player.nameFirst} ${player.nameLast}</p>
-              <p class="round-history-round-data-score"><span>${player.holes[17].overUnderRound}</span> from ${player.holes[17].throwsRound} throws</p>`).join('\n').replaceAll('"', '')}
+              <p class="round-history-round-data-score"><span>${player.finalScore}</span> from ${player.finalThrows} throws</p>`).join('\n').replaceAll('"', '')}
             </div>
           </div>`;
       });
