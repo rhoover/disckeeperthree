a72b34146ed7d0677699c1bd5bfce6510af22e8c
rhoover <robin.hoover@gmail.com>
2022-11-26 16:38:06 -0500

    Working Service Worker

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = function(eleventyConfig) {
 
@@ -7,6 +8,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/img');
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy('favicon.ico');
+  eleventyConfig.addPassthroughCopy('disckeeper-service-worker-min.js');
 
   // add link shortcut to perfectly revisioned css file
   eleventyConfig.addNunjucksShortcode("cssLink", function() {
@@ -17,6 +19,46 @@ module.exports = function(eleventyConfig) {
     return `<link rel="stylesheet" type="text/css" media='print' href="/css/${cssFile}" onload="this.media='screen' " >`;
   });
 
+  // create the disckeeper-sw-data.json file for the service worker
+  eleventyConfig.on('eleventy.after', async () => {
+
+    // endpoints and initialization
+    let foldersToInclude = [ 'css', 'fonts', 'js', 'img/sprites'];    // only html from root
+    let filesToInclude = fs.readdirSync('dist').filter(function(file) {
+      return path.extname(file) === '.html';
+    });
+
+    // go through each endpoint folder to extract each file with file-path to stuff into final array
+    foldersToInclude.forEach((folder) => {
+      let tempFile = fs.readdirSync('dist/' + folder);
+      tempFile.forEach((file) => {
+          filesToInclude.push('/' + folder + '/' + file);
+      });
+    });
+
+    // cleaning up
+    // get only the image in the header
+    let headerImage = fs.readdirSync('dist/img').filter(function(file) {
+      return file === 'disckeeper-logo.webp';
+    });
+    filesToInclude.push('/img/dk-icns/' + headerImage[0]);
+    // not a file but a directory, so get rid of it
+    const index = filesToInclude.indexOf("/fonts");
+    if (index > -1) {
+      filesToInclude.splice(index, 1);
+    }
+    // no need to cache this, so get rid of it
+    // const indexOne = filesToInclude.indexOf('googleeea66bb2e7056d77.html');
+    // if (index > -1) {
+    //   filesToInclude.splice(indexOne, 1);
+    // }
+
+    // write file containing array of files with their path
+    fs.writeFileSync(
+      'dist/disckeeper-service-worker-data.json',
+      JSON.stringify(filesToInclude)
+    )
+  });
   // initialize directories
   return {
     dir: {
---
+++ disckeeper-service-worker.js
@@ -0,0 +1,98 @@
+let timestamp = Date.now();
+let cacheName = 'dkCache-' + timestamp;
+
+//get the array of urls
+async function getServiceWorkerData() {
+  const url = 'disckeeper-service-worker-data.json';
+  const response = await fetch(url, {cache: 'no-store'});
+  const files = await response.json();
+  return files;
+};
+
+self.addEventListener('install', event => {
+
+});
+
+self.addEventListener('activate', event => {
+
+  // Delete all old caches.
+  const removeOldCaches = async () => {
+    caches.keys()
+      .then(function(keys) {
+        for ( let key of keys) {
+          if (key != cacheName) {
+            caches.delete(key);
+          };
+        };
+      });
+    };
+    event.waitUntil(removeOldCaches());
+
+
+  // on with the show
+  const seedCache = async () => {
+    getServiceWorkerData()
+      .then(files => {
+        console.log(files);
+        caches.open(cacheName)
+        .then(cache => {
+          return cache.addAll(files);
+        });
+      });
+  };
+
+  event.waitUntil(seedCache());
+  
+  // let her take over
+  event.waitUntil(clients.claim());
+
+});
+
+self.addEventListener('fetch', event => {
+  const {request} = event;
+  const url = new URL(request.url);
+
+  // Only handle GET requests.
+  if (request.method !== 'GET') return;
+
+  // Don't handle BrowserSync requests.
+  if (url.pathname.startsWith('/browser-sync/')) return;
+
+  // Don't handle non-http requires such as data: URIs.
+  if (!url.protocol.startsWith('http')) return;
+
+  // blacklist to keep cache clean
+  if (url.pathname.startsWith('/bphoto/')) return; // yelp images, remaining are all google maps stuff
+  if (url.origin === 'https://fonts.gstatic.com') return;
+  if (url.origin === 'https://fonts.googleapis.com') return;
+  if (url.origin === 'https://maps.googleapis.com') return;
+  if (url.origin === 'https://maps.gstatic.com') return;
+  if (url.origin === 'https://maps.google.com') return;
+  if (url.pathname.startsWith('/mapfiles/')) return;
+
+  async function getResponsePromise() {
+    const cache = await caches.open(cacheName);
+
+    // Try to find response in the cache.
+    let response = await cache.match(request);
+
+    if (!response) {
+      if (request.cache === 'only-if-cached') return;
+
+      // Try to fetch response from the network.
+      // We will get a 404 error if not found.
+      response = await fetch(request, {cache: 'no-store'});
+      console.info('disckeeper got', url.pathname, 'from network');
+
+      // Cache the response.
+      cache.put(request, response.clone());
+    } else {
+      console.info('disckeeper got', url.pathname, 'from', cacheName);
+    }
+
+    return response;
+  }
+
+  event.respondWith(getResponsePromise());
+
+});
